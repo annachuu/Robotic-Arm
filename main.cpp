e11abdaf339cc218069cc0359b6272c0415ed1eb
anna <annachu211@gmail.com>
2025-11-17 15:44:19 -0600

    setup file

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+/*
+  CS 207 - Final Project
+  Members: Anna Chu, Rojen Bolito, Aimee Nguyen
+  Due:     December 1, 2025
+*/
 
+/* Global Variables */
+
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  
+}  
